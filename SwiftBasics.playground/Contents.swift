c99f4f2b6669dc1ed0964bf5b299fd7a08f615f3
keepcalmandcodecodecode <easy.dev@yandex.ru>
2015-12-25 10:49:45 +0300

    sleepIn
    http://codingbat.com/prob/p187868

--- SwiftBasics.playground/Contents.swift
+++ SwiftBasics.playground/Contents.swift
@@ -7,3 +7,18 @@ let strHello = "Hello "
 let strSwift = "Swift!"
 print(strHello+strSwift)
 
+//CodingBat: warmup 1
+
+//sleepIn
+
+func sleepIn(weekday:Bool,vacation:Bool)->Bool{
+    return !weekday || vacation;
+}
+
+//true
+sleepIn(false, vacation: false)
+//false
+sleepIn(true, vacation: false)
+//true
+sleepIn(false, vacation: true)
+
