1a59f9eeab9f6d3c72642c1bee1b9cfec9b64069
keepcalmandcodecodecode <igor.github@gmail.com>
2017-05-15 15:29:28 +0300

    Change folder structure
    Move playground to separate folder,
    add new project

---
+++ DifferentAlgoritmsImplementations/DifferentAlgoritmsImplementations/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  DifferentAlgoritmsImplementations
+//
+//  Created by dev on 15/05/2017.
+//  Copyright © 2017 easy. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
